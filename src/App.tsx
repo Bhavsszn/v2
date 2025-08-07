d4b99ea36b512efa8fbfca1bea72fef31f963074
Bhavya Sharma <115432614+Bhavsszn@users.noreply.github.com>
2025-08-07 14:34:06 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Header } from './components/Header';
 import { WelcomeScreen } from './components/WelcomeScreen';
 import { SearchScreen } from './components/SearchScreen';
